567d41398160735d0b392d0f94e3a160c0f2e2f3
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2019-04-01 11:12:01 +0100

    improvements to the training plot

--- train_plot.py
+++ train_plot.py
@@ -8,9 +8,13 @@ fname = 'train.log'
 print('Reading:', fname)
 df = pd.read_csv(fname)
 
-epoch = df['epoch'].values
+epoch = df['epoch'].values + 1
 loss = df['loss'].values
 val_loss = df['val_loss'].values
+
+print('epochs:   %8d ... %d' % (np.min(epoch), np.max(epoch)))
+print('loss:     %.6f ... %.6f' % (np.min(loss), np.max(loss)))
+print('val_loss: %.6f ... %.6f' % (np.min(val_loss), np.max(val_loss)))
 
 plt.plot(epoch, loss, label='loss')
 plt.plot(epoch, val_loss, label='val_loss')
@@ -35,7 +39,7 @@ plt.plot([x_min, min_val_epoch], [min_val_loss, min_val_loss], 'k--')
 plt.plot([min_val_epoch, min_val_epoch], [y_min, min_val_loss], 'k--')
 
 plt.xlim(0, x_max)
-plt.ylim(y_min, y_max)
+plt.ylim(y_min, 0.0225)
 
 plt.tight_layout()
 plt.show()
