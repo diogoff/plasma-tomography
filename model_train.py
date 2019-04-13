833302614efba84290fb3b75321a7d61d121af7c
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2019-04-14 00:12:11 +0100

    changed batch size

--- model_train.py
+++ model_train.py
@@ -103,7 +103,7 @@ class MyCallback(Callback):
 
 # ----------------------------------------------------------------------
 
-batch_size = 797
+batch_size = 465
 print('batch_size:', batch_size)
 
 batch_ratio_train = float(X_train.shape[0]) / float(batch_size)
