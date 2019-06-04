b49ec29b5afb9618862a83f7fe7379714c4d04a2
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2019-06-04 13:08:28 +0100

    added the possibility of specifying regions

--- model_valid.py
+++ model_valid.py
@@ -1,7 +1,31 @@
 from __future__ import print_function
 
+import sys
 import numpy as np
+import matplotlib.pyplot as plt
 from skimage.measure import *
+
+# ----------------------------------------------------------------------
+
+i0 = 0
+if len(sys.argv) > 1:
+    i0 = int(sys.argv[1])
+    print('i0:', i0)
+
+i1 = 0
+if len(sys.argv) > 2:
+    i1 = int(sys.argv[2])
+    print('i1:', i1)
+
+j0 = 0
+if len(sys.argv) > 3:
+    j0 = int(sys.argv[3])
+    print('j0:', j0)
+
+j1 = 0
+if len(sys.argv) > 4:
+    j1 = int(sys.argv[4])
+    print('j1:', j1)
 
 # ----------------------------------------------------------------------
 
@@ -52,6 +76,9 @@ for (X_pred, Y_true) in [(X_valid, Y_valid), (X_test, Y_test)]:
     for i in range(X_pred.shape[0]):
         y0 = np.clip(Y_true[i], 0., 1.)
         y1 = np.clip(Y_pred[i], 0., 1.)
+        if (i0 != 0) or (i1 != 0) or (j0 != 0) or (j1 != 0):
+            y0 = y0[i0:i1,j0:j1]
+            y1 = y1[i0:i1,j0:j1]
         ssim.append(compare_ssim(y0, y1))
         psnr.append(compare_psnr(y0, y1))
         rmse.append(compare_nrmse(y0, y1))
